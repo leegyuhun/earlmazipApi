618455a7b62aadca67cb78cd054ac7cbb6374bd4
leegyuhun <79880216+leegyuhun@users.noreply.github.com>
2022-10-06 22:41:15 +0900

    [22.10.06] EarlmazipApi Project First Commit.

---
+++ src/main/java/api/earlmazip/earlmazipApi/EarlmazipApiApplication.java
@@ -0,0 +1,13 @@
+package api.earlmazip.earlmazipApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlmazipApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarlmazipApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/earlmazip/earlmazipApi/EarlmazipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.earlmazip.earlmazipApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarlmazipApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
